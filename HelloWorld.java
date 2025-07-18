76a5e2221d611851c41f734c4cea1c4bd80190d0
BinduMiryala <bindumiryala2605@gmail.com>
2025-07-18 16:02:25 +0530

    Resolved merge conflict in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello Jenkins CI/CD!");
-=======
-        System.out.println("Hello, Jenkins CI/CD!");
->>>>>>> 340b264 (Saved local changes before pulling)
     }
 }
